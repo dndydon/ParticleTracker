f4a75ef4c1e500b8542a415b6730706d971eec0a
Don Sleeter <don@sleeter.com>
2018-01-01 18:19:27 -0800

    Update Swift Package to 4.0

--- Package.swift
+++ Package.swift
@@ -1,10 +1,28 @@
-//
-//  Package.swift
-//  ParticleTracker
-//
-//  Created by Don Sleeter on 7/28/17.
-//  Copyright © 2017 Don Sleeter. All rights reserved.
-//
+// swift-tools-version:4.0
+// The swift-tools-version declares the minimum version of Swift required to build this package.
 
-// stub
+import PackageDescription
 
+let package = Package(
+    name: "ParticleTracker",
+    products: [
+        // Products define the executables and libraries produced by a package, and make them visible to other packages.
+        .library(
+            name: "ParticleTracker",
+            targets: ["ParticleTracker"]),
+    ],
+    dependencies: [
+        // Dependencies declare other packages that this package depends on.
+        // .package(url: /* package url */, from: "1.0.0"),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
+        .target(
+            name: "ParticleTracker",
+            dependencies: []),
+        .testTarget(
+            name: "ParticleTrackerTests",
+            dependencies: ["ParticleTracker"]),
+    ]
+)
